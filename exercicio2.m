f7aef2fa56ef9e652afc8b44815d4252f066a397
Giovana Tabosa <gittabosa@gmail.com>
2025-09-06 18:46:37 -0300

    ex 2 modificado

--- exercicio2.m
+++ exercicio2.m
@@ -10,30 +10,24 @@ pkg load optim
 % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
-
-f = [10,2*NG];
-x0 = [1,1]';
-
-A = [-0.30, -0.20;
-    -0.20, -0.25;
-    -0.25, -0.30];
-
-b = [-15;
-    -10;
-    -12];
-
-Aeq = [];
-beq = [];
-
-ub = [20; 50];
-lb = [0; 0];
-
-x = linprog(f, A, b, Aeq, beq, lb, ub);
-x = x'; 
+        
+ f = [10, 2*NG];
+    
+    A = [0.30, 0.20;
+    0.20, 0.25;
+    0.25, 0.30];   
+    
+ A = -A;  % Multiplicando por -1 para converter ≥ para ≤
+ b = [-15; -10; -12];  % Lado direito já negativo
+    
+ub = [20, 50];
+lb = [0, 0];
+    
+op = optimset('Display', 'off');
+x = linprog(f, A, b, [], [], lb, ub, [], options);
 
 
-
-% mantenha essas duas linhas finais
+ % mantenha essas duas linhas finais
 blendA = x(1);
 blendB = x(2);
 
