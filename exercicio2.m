c2e7705d3b9f74969876bb5b8871e406ece7e5bb
Giovana Tabosa <gittabosa@gmail.com>
2025-09-06 20:02:23 -0300

    ex 2 mod

--- exercicio2.m
+++ exercicio2.m
@@ -1,16 +1,23 @@
 function [blendA, blendB] = exercicio2(NG)
 
-NG = 4; % NG = número do grupo
+% NG: numero do grupo
+NG = 5;
 
-f = [10, 2*NG]';
+% nao alterar: inicio
+es = 1;
+imax = 20; 
+% nao alterar: fim
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-x0 = [1,1]';
-
+% digite seu codigo aqui
+f = [10,2*NG];
+x0 = [1,1]';'
 
 A = [-0.30, -0.20;
     -0.20, -0.25;
     -0.25, -0.30];
+
 b = [-15;
     -10;
     -12];
@@ -21,8 +28,15 @@ beq = [];
 ub = [20; 50];
 lb = [0; 0];
 
-x = linprog(f,A,b,Aeq,beq,lb,ub);
-x = x'; 
+x = linprog(f, A, b, Aeq, beq, lb, ub);
+x = x';
 
+
+% mantenha essas duas linhas finais
+
+blendA = x(1);
+blendB = x(2);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 endfunction
