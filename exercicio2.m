f4d0ae18a507d57da3f73134b42bfb8d91cadb42
Giovana Tabosa <gittabosa@gmail.com>
2025-09-06 18:05:22 -0300

    modificado ex 2

--- exercicio2.m
+++ exercicio2.m
@@ -1,6 +1,7 @@
 function [blendA, blendB] = exercicio2(NG)
 
 % NG: numero do grupo
+NG = 5;
 
 % nao alterar: inicio
 es = 1;
@@ -10,7 +11,26 @@ pkg load optim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
+f = [10,2*NG];
+x0 = [1,1]';
+
+A = [-0.30, -0.20;
+    -0.20, -0.25;
+    -0.25, -0.30];
+
+b = [-15;
+    -10;
+    -12];
+
+Aeq = [];
+beq = [];
+
+ub = [20; 50];
+lb = [0; 0];
+
+x = linprog(f, A, b, Aeq, beq, lb, ub);
+x = x'; 
+
 
 
 % mantenha essas duas linhas finais
