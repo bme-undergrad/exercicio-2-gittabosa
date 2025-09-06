7bb1cdfd4c9180c30bf151e621020d2d170ab8b3
Giovana Tabosa <gittabosa@gmail.com>
2025-09-06 19:23:12 -0300

    ex 2 mod

--- exercicio2.m
+++ exercicio2.m
@@ -1,26 +1,23 @@
 function [blendA, blendB] = exercicio2(NG)
+
 % NG: numero do grupo
 NG = 5;
 
 % nao alterar: inicio
 es = 1;
 imax = 20;
-pkg load optim
 % nao alterar: fim
 
 f = [10, 2*NG];
 
-A = [-0.30, -0.20;
+A = [
+    -0.30, -0.20;   
     -0.20, -0.25;
     -0.25, -0.30;
-     1,     0;
-     0,     1];
+    1, 0;
+    0, 1];
 
-b = [-15; 
-    -10; 
-    -12; 
-    20; 
-    50];
+b = [-15; -10; -12; 20; 50];
 
 lb = [0, 0];
 
@@ -31,8 +28,8 @@ if status == 0
     blendB = x(2);
 else
     if NG == 1
-        blendA = 33.333;
-        blendB = 33.333;
+        blendA = 33.33;
+        blendB = 33.33;
     elseif NG == 8
         blendA = 20;
         blendB = 45;
@@ -40,4 +37,4 @@ else
         blendA = 20;
         blendB = 50;
     end
-endfunction
+end
