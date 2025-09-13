bb7b5d8333b036ec9caa5f09a44870a14f5f4f6b
gittabosa <gittabosa@gmail.com>
2025-09-13 20:16:18 -0300

    Update exercicio2.m

--- exercicio2.m
+++ exercicio2.m
@@ -2,30 +2,26 @@ function [blendA, blendB] = exercicio2(NG)
 
   % NG: numero do grupo
 
-% nao alterar: inicio
-es = 1;
-imax = 20; 
-pkg load optim
-% nao alterar: fim
+  % nao alterar: inicio
+  es = 1;
+  imax = 20;
+  pkg load optim
+  % nao alterar: fim
 
   %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-  % Custos
-  c = [10, 2*NG];   % custo do blendA e blendB
+custos = [10, 2*NG];   % custo do blendA e blendB
 
-  % Restricoes (A * x >= b)
-  A = [-0.3, -0.2;   % polietileno
+A = [-0.3, -0.2;   % polietileno
        -0.2, -0.25;  % PLA
        -0.25, -0.3]; % polipropileno
 
-  b = [-15; -10; -12];
+b = [-15; -10; -12];
 
-  % limites das variáveis
-  lb = [0; 0];
-  ub = [20; 50];
+lb = [0; 0];
+ub = [20; 50];
 
-  % resolve PL
-  [x, fval, info] = glpk(c, A, b, lb, ub, "UUU", "CCC");
+[x, fval, info] = glpk(c, A, b, lb, ub, "UUU", "CCC");
 
   %%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -36,3 +32,26 @@ pkg load optim
   %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 endfunction
+s = [10, 2*NG];   % custo do blendA e blendB
+
+  A = [-0.3, -0.2;   % polietileno
+       -0.2, -0.25;  % PLA
+       -0.25, -0.3]; % polipropileno
+
+  b = [-15; -10; -12];
+
+  lb = [0; 0];
+  ub = [20; 50];
+
+[x, fval, info] = glpk(c, A, b, lb, ub, "UUU", "CCC");
+
+  %%%%%%%%%%%%%%%%%%%%%%%%%%
+
+  % mantem essas duas linhas finais
+  blendA = x(1);
+  blendB = x(2);
+
+  %%%%%%%%%%%%%%%%%%%%%%%%%%
+
+endfunction
+
