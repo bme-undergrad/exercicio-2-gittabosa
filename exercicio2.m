30c3ca890894ad497655615ff2374631a895dd51
gittabosa <gittabosa@gmail.com>
2025-09-13 20:31:17 -0300

    Update exercicio2.m

--- exercicio2.m
+++ exercicio2.m
@@ -10,17 +10,23 @@ function [blendA, blendB] = exercicio2(NG)
 
   %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-  f = [10, 2*NG];   % custo de blend A e blend B
+  % Custos (minimizar custo total)
+  c = [10; 2*NG];   % <- vetor coluna
 
+  % Restricoes do tipo A*x <= b
   A = [-0.3, -0.2;   % polietileno >= 15
        -0.2, -0.25;  % PLA >= 10
        -0.25, -0.3]; % polipropileno >= 12
 
-  b = [-15; -10; -12];
+  b = [-15; 
+      -10; 
+      -12];
 
+  % Limites das variáveis
   lb = [0; 0];
   ub = [20; 50];
 
+  % resolve o problema de PL
   [x, fval, info] = linprog(c, A, b, [], [], lb, ub);
 
   %%%%%%%%%%%%%%%%%%%%%%%%%%
