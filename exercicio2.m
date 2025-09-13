d99a6e78cbad768de5ad993bacff1103f397599a
gittabosa <gittabosa@gmail.com>
2025-09-13 20:17:18 -0300

    Update exercicio2.m

--- exercicio2.m
+++ exercicio2.m
@@ -10,6 +10,7 @@ function [blendA, blendB] = exercicio2(NG)
 
   %%%%%%%%%%%%%%%%%%%%%%%%%%
 
+NG = 5;
 custos = [10, 2*NG];   % custo do blendA e blendB
 
 A = [-0.3, -0.2;   % polietileno
@@ -32,26 +33,3 @@ ub = [20; 50];
   %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 endfunction
-s = [10, 2*NG];   % custo do blendA e blendB
-
-  A = [-0.3, -0.2;   % polietileno
-       -0.2, -0.25;  % PLA
-       -0.25, -0.3]; % polipropileno
-
-  b = [-15; -10; -12];
-
-  lb = [0; 0];
-  ub = [20; 50];
-
-[x, fval, info] = glpk(c, A, b, lb, ub, "UUU", "CCC");
-
-  %%%%%%%%%%%%%%%%%%%%%%%%%%
-
-  % mantem essas duas linhas finais
-  blendA = x(1);
-  blendB = x(2);
-
-  %%%%%%%%%%%%%%%%%%%%%%%%%%
-
-endfunction
-
